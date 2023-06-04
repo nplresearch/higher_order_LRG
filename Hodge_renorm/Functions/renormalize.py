Fixed number of steps in specific heat computation

Hodge_renorm/Functions/renormalize.py
@@ -156,7 +156,9 @@ def renormalize_simplicial_VARIANTS(
                 ]  # Collapse node i to the representative
     elif METHOD == "closest":
         G = nx.Graph()
-        G.add_edges_from([(e[0],e[1]) for e in sc["edges"]])  # using a list of edge tuples
+        G.add_edges_from(
+            [(e[0], e[1]) for e in sc["edges"]]
+        )  # using a list of edge tuples
         for i in range(sc["n0"]):
             if len(nodesclusters[i]) == 1:  # The node is not an interface
                 if (
@@ -169,7 +171,10 @@ def renormalize_simplicial_VARIANTS(
                     setint = clusternodesinterface[list(nodesclusters[i])[0]] + [
                         cluster_representatives[list(nodesclusters[i])[0]]
                     ]
-                dist = [ nx.algorithms.shortest_path_length(G,source = i,target = x) for x in setint]
+                dist = [
+                    nx.algorithms.shortest_path_length(G, source=i, target=x)
+                    for x in setint
+                ]
                 id = np.argmin(dist)
                 mapnodes[i] = mapnodes[
                     setint[id]
@@ -190,10 +195,12 @@ def renormalize_simplicial_VARIANTS(
         if node1 != node2:
             new_edges.append([node1, node2])
     if len(new_edges) != 0:
-        new_sc["edges"] = np.unique(np.sort(np.array(new_edges,dtype = int), axis=1), axis=0)
+        new_sc["edges"] = np.unique(
+            np.sort(np.array(new_edges, dtype=int), axis=1), axis=0
+        )
         new_sc["n1"] = new_sc["edges"].shape[0]
     else:
-        new_sc["edges"] = np.zeros((0, 2),dtype = int)
+        new_sc["edges"] = np.zeros((0, 2), dtype=int)
         new_sc["n1"] = 0
 
     # Connect supernodes with faces
@@ -205,10 +212,12 @@ def renormalize_simplicial_VARIANTS(
         if len(np.unique([node1, node2, node3])) == 3:
             new_faces.append([node1, node2, node3])
     if len(new_faces) != 0:
-        new_sc["faces"] = np.unique(np.sort(np.array(new_faces,dtype = int), axis=1), axis=0)
+        new_sc["faces"] = np.unique(
+            np.sort(np.array(new_faces, dtype=int), axis=1), axis=0
+        )
         new_sc["n2"] = new_sc["faces"].shape[0]
     else:
-        new_sc["faces"] = np.zeros((0, 3),dtype = int)
+        new_sc["faces"] = np.zeros((0, 3), dtype=int)
         new_sc["n2"] = 0
 
     # Connect supernodes with tetrahedra
@@ -222,11 +231,11 @@ def renormalize_simplicial_VARIANTS(
             new_tetrahedra.append([node1, node2, node3, node4])
     if len(new_tetrahedra) != 0:
         new_sc["tetrahedra"] = np.unique(
-            np.sort(np.array(new_tetrahedra,dtype = int), axis=1), axis=0
+            np.sort(np.array(new_tetrahedra, dtype=int), axis=1), axis=0
         )
         new_sc["n3"] = new_sc["tetrahedra"].shape[0]
     else:
-        new_sc["tetrahedra"] = np.zeros((0, 4),dtype = int)
+        new_sc["tetrahedra"] = np.zeros((0, 4), dtype=int)
         new_sc["n3"] = 0
 
     return new_sc, mapnodes, comp, nodesclusters

Hodge_renorm/Functions/scomplex.py
@@ -160,9 +160,9 @@ def NGF_d1(N, s, beta):
     G = nx.from_numpy_matrix(a)
     sc = {
         "nodes": np.arange(0, N),
-        "edges": np.array(list(G.edges()),dtype = int),
-        "faces": np.array([],dtype = int),
-        "tetrahedra": np.array([],dtype = int),
+        "edges": np.array(list(G.edges()), dtype=int),
+        "faces": np.array([], dtype=int),
+        "tetrahedra": np.array([], dtype=int),
     }
     sc["n0"] = N
     sc["n1"] = sc["edges"].shape[0]
@@ -185,9 +185,9 @@ def NGF_d2(N, s, beta):
     a_occ2 = csr_matrix((N, N))
     sc = {
         "nodes": np.arange(0, N),
-        "edges": np.array([],dtype = int),
-        "faces": np.array([],dtype = int),
-        "tetrahedra": np.array([],dtype = int),
+        "edges": np.array([], dtype=int),
+        "faces": np.array([], dtype=int),
+        "tetrahedra": np.array([], dtype=int),
     }
 
     # Assign energies to the nodes
@@ -300,9 +300,9 @@ def NGF_d3(N, s, beta):
     a_occ3 = np.zeros((0))
     sc = {
         "nodes": np.arange(0, N),
-        "edges": np.array([],dtype = int),
-        "faces": np.zeros((0, 3),dtype = int),
-        "tetrahedra": np.array([],dtype = int),
+        "edges": np.array([], dtype=int),
+        "faces": np.zeros((0, 3), dtype=int),
+        "tetrahedra": np.array([], dtype=int),
     }
 
     # Assign energies to the nodes

Hodge_renorm/stat_scan.py
@@ -10,7 +10,7 @@ from scipy.io import savemat
 from scipy.signal import find_peaks
 from scipy.sparse.linalg import eigsh
 
-# Pass arguments from command line:  
+# Pass arguments from command line:
 # python stat_scan.py N d n_tau rep METHOD SPARSIFY TRUE_CONNECTIONS factor0 factor1 factor2 factor3
 # It is advised to have factor0 = factor1 = factor2 = 1 and factor3 = 2 or 2.5
 
@@ -20,7 +20,7 @@ n_tau = int(sys.argv[3])  # 100
 rep = int(sys.argv[4])  # 10
 METHOD = sys.argv[5]  # {"representative","closest"}
 SPARSIFY = sys.argv[6] == "True"  # False
-TRUE_CONNECTIONS = sys.argv[7] == "True" # True
+TRUE_CONNECTIONS = sys.argv[7] == "True"  # True
 factor0 = int(sys.argv[8])
 factor1 = int(sys.argv[9])
 factor2 = int(sys.argv[10])
@@ -51,7 +51,7 @@ for r in range(rep):
     D0, U0 = eigsh(L0, k=Na, which="SM")
     D0 = np.concatenate((D0, 10000 * np.ones(sc["n0"] - Na)))
     U0 = np.concatenate((U0, np.zeros((sc["n0"], sc["n0"] - Na))), axis=1)
-    [specific_heat, tau_space] = renormalize.compute_heat(D0, -2, 1, 200)
+    [specific_heat, tau_space] = renormalize.compute_heat(D0, -2, 1.5, n_tau)
     id, __ = find_peaks(specific_heat)
     tau_max0 = tau_space[id[0]] / factor0
     tau_space0 = np.linspace(0, tau_max0, n_tau)
@@ -63,7 +63,7 @@ for r in range(rep):
         D1, U1 = eigsh(L1, k=Na, which="SM")
         D1 = np.concatenate((D1, 10000 * np.ones(sc["n1"] - Na)))
         U1 = np.concatenate((U1, np.zeros((sc["n1"], sc["n1"] - Na))), axis=1)
-        [specific_heat, tau_space] = renormalize.compute_heat(D1, -2, 2, 200)
+        [specific_heat, tau_space] = renormalize.compute_heat(D1, -2, 1.5, n_tau)
         id, __ = find_peaks(specific_heat)
         tau_max1 = tau_space[id[0]] / factor1
         tau_space1 = np.linspace(0, tau_max1, n_tau)
@@ -75,7 +75,7 @@ for r in range(rep):
         D2, U2 = eigsh(L2, k=Na, sigma=0.0, which="LM")
         D2 = np.concatenate((D2, 10000 * np.ones(sc["n2"] - Na)))
         U2 = np.concatenate((U2, np.zeros((sc["n2"], sc["n2"] - Na))), axis=1)
-        [specific_heat, tau_space] = renormalize.compute_heat(D2, -2, 2, 200)
+        [specific_heat, tau_space] = renormalize.compute_heat(D2, -2, 1.5, n_tau)
         id, __ = find_peaks(specific_heat)
         tau_max2 = tau_space[id[0]] / factor2
         tau_space2 = np.linspace(0, tau_max2, n_tau)
@@ -87,7 +87,7 @@ for r in range(rep):
         D3, U3 = eigsh(L3, k=Na, which="SM")
         D3 = np.concatenate((D3, 10000 * np.ones(sc["n3"] - Na)))
         U3 = np.concatenate((U3, np.zeros((sc["n3"], sc["n3"] - Na))), axis=1)
-        [specific_heat, tau_space] = renormalize.compute_heat(D3, -2, 2, 200)
+        [specific_heat, tau_space] = renormalize.compute_heat(D3, -2, 1.5, n_tau)
         id, __ = find_peaks(specific_heat)
         tau_max3 = tau_space[id[0]] / factor3
         tau_space3 = np.linspace(0, tau_max3, n_tau)