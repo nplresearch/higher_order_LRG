Fixed computation of specific heat

Hodge_renorm/Functions/scomplex.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 import scipy.sparse as sp
-from scipy.sparse import csr_matrix, lil_matrix
+from scipy.sparse import csr_matrix, lil_matrix, tril
 
 
 def make_dict(sc):
@@ -196,15 +196,15 @@ def NGF_d2(N, s, beta):
 
     # Initial condition at time t=1 including a single triangle between nodes 1, 2, 3
     L = 0
-    for i1 in range(1, 4):
-        for i2 in range(i1 + 1, 4):
+    for i1 in range(3):
+        for i2 in range(i1 + 1,3):
             L += 1
-            a[i1 - 1, i2 - 1] = np.exp(-beta * (epsilon[i1 - 1] + epsilon[i2 - 1]))
-            a[i2 - 1, i1 - 1] = np.exp(-beta * (epsilon[i1 - 1] + epsilon[i2 - 1]))
-            a_occ[i1 - 1, i2 - 1] = 1
-            a_occ[i2 - 1, i1 - 1] = 1
-            a_occ2[i1 - 1, i2 - 1] = 1
-            a_occ2[i2 - 1, i1 - 1] = 1
+            a[i1, i2] = np.exp(-beta * (epsilon[i1] + epsilon[i2]))
+            a[i2, i1] = np.exp(-beta * (epsilon[i1] + epsilon[i2]))
+            a_occ[i1, i2] = 1
+            a_occ[i2, i1] = 1
+            a_occ2[i1, i2] = 1
+            a_occ2[i2, i1] = 1
 
     nt = 1
     sc["faces"] = np.array([[0, 1, 2]])
@@ -220,9 +220,10 @@ def NGF_d2(N, s, beta):
         theta[i] = i / 3
 
     # At each time t=in-2 we attach a new triangle
-    for in_ in range(3 + 1, N + 1):
+    for in_ in range(3, N):
         # Choose edge (l1,l2) to which we will attach the new triangle
-        mat = csr_matrix(a.multiply(a_occ))
+        
+        mat = tril(a.multiply(a_occ),format = "csr")
         I, J = mat.nonzero()
         V = np.squeeze(mat[mat.nonzero()])
         norm = np.sum(V)
@@ -236,17 +237,17 @@ def NGF_d2(N, s, beta):
             l1 = I[nj]
             l2 = J[nj]
 
-            d_N[in_ - 1] = min(d_N[l1], d_N[l2]) + 1
-            D[in_ - 1] = max(d_N)
-            r[in_ - 1, :] = r[l1, :] + r[l2, :]
-            r[in_ - 1, :] /= np.sqrt(r[in_ - 1, 0] ** 2 + r[in_ - 1, 1] ** 2)
-            theta[in_ - 1] = min(theta[l1], theta[l2]) + 0.5 * abs(
+            d_N[in_] = min(d_N[l1], d_N[l2]) + 1
+            D[in_] = max(d_N)
+            r[in_, :] = r[l1, :] + r[l2, :]
+            r[in_ , :] /= np.sqrt(r[in_ , 0] ** 2 + r[in_ , 1] ** 2)
+            theta[in_] = min(theta[l1], theta[l2]) + 0.5 * abs(
                 theta[l1] - theta[l2]
             )
             if theta[l1] == 0 and theta[l2] >= 2 / 3:
-                theta[in_ - 1] = theta[l2] + 0.5 * (1 - theta[l2])
+                theta[in_] = theta[l2] + 0.5 * (1 - theta[l2])
             if theta[l2] == 0 and theta[l1] >= 2 / 3:
-                theta[in_ - 1] = theta[l1] + 0.5 * (1 - theta[l1])
+                theta[in_] = theta[l1] + 0.5 * (1 - theta[l1])
 
             a_occ[l1, l2] += s
             a_occ[l2, l1] += s
@@ -254,27 +255,27 @@ def NGF_d2(N, s, beta):
             a_occ2[l2, l1] += 1
 
             nt += 1
-            sc["faces"] = np.concatenate((sc["faces"], np.array([[in_ - 1, l1, l2]])))
+            sc["faces"] = np.concatenate((sc["faces"], np.array([[in_, l1, l2]])))
 
             # Attach the new node in to the node l1
             L += 1
-            a[in_ - 1, l1] = np.exp(-beta * (epsilon[l1] + epsilon[in_ - 1]))
-            a[l1, in_ - 1] = np.exp(-beta * (epsilon[l1] + epsilon[in_ - 1]))
-            a_occ[in_ - 1, l1] = 1
-            a_occ[l1, in_ - 1] = 1
-            a_occ2[in_ - 1, l1] = 1
-            a_occ2[l1, in_ - 1] = 1
+            a[in_, l1] = np.exp(-beta * (epsilon[l1] + epsilon[in_]))
+            a[l1, in_] = np.exp(-beta * (epsilon[l1] + epsilon[in_]))
+            a_occ[in_, l1] = 1
+            a_occ[l1, in_] = 1
+            a_occ2[in_, l1] = 1
+            a_occ2[l1, in_] = 1
 
             # Attach the new node in to the node l2
             L += 1
-            a[in_ - 1, l2] = np.exp(-beta * (epsilon[l2] + epsilon[in_ - 1]))
-            a[l2, in_ - 1] = np.exp(-beta * (epsilon[l2] + epsilon[in_ - 1]))
-            a_occ[in_ - 1, l2] = 1
-            a_occ[l2, in_ - 1] = 1
-            a_occ2[in_ - 1, l2] = 1
-            a_occ2[l2, in_ - 1] = 1
+            a[in_, l2] = np.exp(-beta * (epsilon[l2] + epsilon[in_]))
+            a[l2, in_] = np.exp(-beta * (epsilon[l2] + epsilon[in_]))
+            a_occ[in_, l2] = 1
+            a_occ[l2, in_] = 1
+            a_occ2[in_, l2] = 1
+            a_occ2[l2, in_] = 1
 
-    I, J = csr_matrix(a).nonzero()
+    I, J = tril(a,format = "csr").nonzero()
     sc["edges"] = np.column_stack((I, J))
 
     sc["edges"] = np.unique(np.sort(sc["edges"], axis=1), axis=0)

Hodge_renorm/stat_analysis.py
@@ -53,7 +53,6 @@ def plot_mean(Ns, data, col, logscale, ax, lab):
 with open(path + "/deg_dist.pkl", "rb") as f:
     deg_dist = pickle.load(f)
 
-
 Ns = np.zeros((rep, d + 1, n_tau), dtype=int)
 for r in range(rep):
     for i in range(d + 1):
@@ -106,14 +105,14 @@ for i in range(d):
     ax = axv[i]
     for j in range(d + 1):
         lab = "L" + str(j)
-        plot_mean(
-            1 - Ns[:, j, :] / N, deg_distance[:, j, i, :], palette[j, :], ls, ax, lab
-        )
+        #plot_mean(
+        #    1 - Ns[:, j, :] / N, deg_distance[:, j, i, :], palette[j, :], ls, ax, lab
+        #)
         if ls:
             for r in range(rep):
                 ax.semilogy(
-                    1 - Ns[r, j, :] / N,
-                    deg_distance[r, j, i, :],
+                    np.squeeze(1 - Ns[r, j, :]/N),
+                    np.squeeze(deg_distance[r, j, i, :]),
                     linewidth=1,
                     color=palette[j, :],
                     alpha=0.3,
@@ -130,7 +129,7 @@ for i in range(d):
 
     ax.legend()
     ax.set_title(r"\textbf{" + names[i] + "-" + names[d] + "}", fontsize=14)
-    ax.set_xlim([0, 0.8])
+    #ax.set_xlim([0, 0.8])
 
 plt.show()
 plt.savefig(path + "/deg_errors.pdf", format="pdf", bbox_inches="tight")

Hodge_renorm/stat_scan.py
@@ -5,10 +5,11 @@ import sys
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-from Functions import renormalize, scomplex
+from Functions import renormalize, scomplex, plotting
 from scipy.io import savemat
 from scipy.signal import find_peaks
 from scipy.sparse.linalg import eigsh
+import scipy
 
 # Pass arguments from command line:
 # python stat_scan.py N d n_tau rep METHOD SPARSIFY TRUE_CONNECTIONS factor0 factor1 factor2 factor3
@@ -21,10 +22,17 @@ rep = int(sys.argv[4])  # 10
 METHOD = sys.argv[5]  # {"representative","closest"}
 SPARSIFY = sys.argv[6] == "True"  # False
 TRUE_CONNECTIONS = sys.argv[7] == "True"  # True
-factor0 = int(sys.argv[8])
-factor1 = int(sys.argv[9])
-factor2 = int(sys.argv[10])
-factor3 = int(sys.argv[11])
+
+if len(sys.argv) > 8:
+    factor0 = int(sys.argv[8])
+    factor1 = int(sys.argv[9])
+    factor2 = int(sys.argv[10])
+    factor3 = int(sys.argv[11])
+else:
+    factor0 = 1
+    factor1 = 1
+    factor2 = 1
+    factor3 = 1
 
 s = 1
 beta = 0.1
@@ -45,13 +53,16 @@ for r in range(rep):
     B1, B2, B3, edge_dict, face_dict = scomplex.boundary_matrices_3(sc)
 
     # Laplacians
-    L0 = B1 @ B1.T
+    L0 = B1 @ (B1.T)
+
     L0 = L0.asfptype()
-    Na = sc["n0"] - 1
-    D0, U0 = eigsh(L0, k=Na, which="SM")
+    Na = sc["n0"]-1
+    #D0,U0 = scipy.linalg.eig(L0.todense())
+    D0, U0 = eigsh(L0, k=Na, sigma = 0, which="LM")
     D0 = np.concatenate((D0, 10000 * np.ones(sc["n0"] - Na)))
     U0 = np.concatenate((U0, np.zeros((sc["n0"], sc["n0"] - Na))), axis=1)
-    [specific_heat, tau_space] = renormalize.compute_heat(D0, -2, 1.5, n_tau)
+    [specific_heat, tau_space] = renormalize.compute_heat(D0, -2, 1.5, 200)
+
     id, __ = find_peaks(specific_heat)
     tau_max0 = tau_space[id[0]] / factor0
     tau_space0 = np.linspace(0, tau_max0, n_tau)
@@ -63,7 +74,7 @@ for r in range(rep):
         D1, U1 = eigsh(L1, k=Na, which="SM")
         D1 = np.concatenate((D1, 10000 * np.ones(sc["n1"] - Na)))
         U1 = np.concatenate((U1, np.zeros((sc["n1"], sc["n1"] - Na))), axis=1)
-        [specific_heat, tau_space] = renormalize.compute_heat(D1, -2, 1.5, n_tau)
+        [specific_heat, tau_space] = renormalize.compute_heat(D1, -2, 1.5, 200)
         id, __ = find_peaks(specific_heat)
         tau_max1 = tau_space[id[0]] / factor1
         tau_space1 = np.linspace(0, tau_max1, n_tau)
@@ -75,7 +86,7 @@ for r in range(rep):
         D2, U2 = eigsh(L2, k=Na, sigma=0.0, which="LM")
         D2 = np.concatenate((D2, 10000 * np.ones(sc["n2"] - Na)))
         U2 = np.concatenate((U2, np.zeros((sc["n2"], sc["n2"] - Na))), axis=1)
-        [specific_heat, tau_space] = renormalize.compute_heat(D2, -2, 1.5, n_tau)
+        [specific_heat, tau_space] = renormalize.compute_heat(D2, -2, 1.5, 200)
         id, __ = find_peaks(specific_heat)
         tau_max2 = tau_space[id[0]] / factor2
         tau_space2 = np.linspace(0, tau_max2, n_tau)
@@ -87,7 +98,7 @@ for r in range(rep):
         D3, U3 = eigsh(L3, k=Na, which="SM")
         D3 = np.concatenate((D3, 10000 * np.ones(sc["n3"] - Na)))
         U3 = np.concatenate((U3, np.zeros((sc["n3"], sc["n3"] - Na))), axis=1)
-        [specific_heat, tau_space] = renormalize.compute_heat(D3, -2, 1.5, n_tau)
+        [specific_heat, tau_space] = renormalize.compute_heat(D3, -2, 1.5, 200)
         id, __ = find_peaks(specific_heat)
         tau_max3 = tau_space[id[0]] / factor3
         tau_space3 = np.linspace(0, tau_max3, n_tau)
@@ -128,7 +139,7 @@ for r in range(rep):
                 D = D3
                 L = L3
 
-            new_sc, mapnodes, comp, __ = renormalize.renormalize_simplicial_VARIANTS(
+            new_sc, mapnodes, comp, __ = renormalize.renormalize_simplicial_VARIANTS_cut(
                 sc, order, L, U, D, tau_space[t], METHOD, SPARSIFY, TRUE_CONNECTIONS
             )
             new_edge_dict, new_face_dict = scomplex.make_dict(new_sc)

Hodge_renorm/test.py
@@ -1,55 +1,138 @@
-import time
+import pickle
+import sys
 
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-from Functions import plotting, renormalize, scomplex, support
-from scipy.sparse.linalg import eigsh
+import powerlaw as pwl
+import scipy
+from Functions import plotting, scomplex
+from scipy.stats import t
 
-N = 1000
-order = 1
-tau = 3
-METHOD = "representative"
-SPARSIFY = False
-TRUE_CONNECTIONS = True
-start = time.time()
-sc = scomplex.NGF_d2(N, 1, 0.1)
-end = time.time()
-print(end - start)
-
-start = time.time()
-B1, B2, B3, __, __ = scomplex.boundary_matrices_3(sc)
-end = time.time()
-print(end - start)
-
-if order == 0:
-    nk = sc["n0"]
-    L = B1 @ (B1.T)
-elif order == 1:
-    nk = sc["n1"]
-    L = (B1.T) @ B1 + B2 @ (B2.T)
-elif order == 2:
-    nk = sc["n2"]
-    L = (B2.T) @ B2 + B3 @ (B3.T)
-elif order == 3:
-    nk = sc["n3"]
-    L = (B3.T) @ B3
-
-L = L.asfptype()
-Na = nk // 2
-D, U = eigsh(L, k=Na, which="SM")
-D = np.concatenate((D, 10000 * np.ones(nk - Na)))
-U = np.concatenate((U, np.zeros((nk, nk - Na))), axis=1)
-
-
-ax = plt.subplot(1, 2, 1)
-plotting.plot_complex(sc, ax)
-start = time.time()
-new_sc, _, _, _ = renormalize.renormalize_simplicial_VARIANTS(
-    sc, order, L, U, D, tau, METHOD, SPARSIFY, TRUE_CONNECTIONS
+plt.rcParams["text.usetex"] = True
+palette = np.array(
+    [
+        [0.3647, 0.2824, 0.1059],
+        [0.8549, 0.6314, 0.3294],
+        [0.4745, 0.5843, 0.5373],
+        [0.4745, 0.3843, 0.7373],
+    ]
 )
-end = time.time()
-print(end - start)
-ax2 = plt.subplot(1, 2, 2)
-plotting.plot_complex(new_sc, ax2)
+
+
+N = 2000
+d =2
+n_tau = 50
+rep = 10
+METHOD = 'representative'
+SPARSIFY = False
+TRUE_CONNECTIONS = False
+
+s = 1
+beta = 0.1
+ls = True  # logscale
+
+pref = f"d{d}s{s}"
+path = f"hodge_renormalization/Hodge_renorm/Tests/Experiments_{METHOD}_{SPARSIFY}_{TRUE_CONNECTIONS}/{pref}"
+
+
+def plot_mean(Ns, data, col, logscale, ax, lab):
+    Nmin = np.min(np.min(Ns))
+    Nmax = np.max(np.max(Ns))
+    xq = np.linspace(Nmin, Nmax, 100)
+    dataq = np.zeros((Ns.shape[0], 100))
+    for r in range(Ns.shape[0]):
+        uNs, uid = np.unique(Ns[r, :], return_index=True)
+        dataq[r, :] = np.interp(xq, uNs, data[r, uid], left=0, right=0)
+    if logscale:
+        ax.semilogy(xq, np.mean(dataq, axis=0), linewidth=2, color=col, label=lab)
+    else:
+        ax.plot(xq, np.mean(dataq, axis=0), linewidth=2, color=col, label=lab)
+
+
+
+with open(path + "/deg_dist.pkl", "rb") as f:
+    deg_dist = pickle.load(f)
+
+print([len(deg_dist[0][t][0][0]) for t in range(50)])
+exit()
+Ns = np.zeros((rep, d + 1, n_tau), dtype=int)
+for r in range(rep):
+    for i in range(d + 1):
+        for t in range(n_tau):
+            Ns[r, i, t] = len(deg_dist[r][t][i][0])
+
+# Ns = sp.io.loadmat(path + '/Ns.mat')['Ns']
+# deg_dist = sp.io.loadmat(fpath pref + '/deg_dist.mat')['deg_dist']
+
+names = ["Node", "Link", "Face", "Tetrahedron"]
+
+
+deg_distance = np.zeros((rep, d + 1, d, n_tau))
+for r in range(rep):
+    for norml in range(d + 1):
+        for degg in range(d):
+            deg1 = deg_dist[r][0][0][degg]
+            for tau in range(n_tau):
+                deg2 = deg_dist[r][tau][norml][degg]
+                if len(deg2) == 0:
+                    deg2 = [0]
+                # KS Distance
+                test = scipy.stats.kstest(deg1, deg2)
+                deg_distance[r, norml, degg, tau] = test.statistic
+                # Powerlaw
+                # fit_function = pwl.Fit(deg2)
+                # deg_distance[r, norml, degg, tau] = fit_function.power_law.D
+
+
+fig = plt.figure()
+
+gs = fig.add_gridspec(d, 2)
+ax1 = fig.add_subplot(gs[:, 0])
+axv = []
+for i in range(d):
+    ax = fig.add_subplot(gs[i, 1])
+    axv = axv + [ax]
+fig.tight_layout()
+
+if d == 1:
+    sc = scomplex.NGF_d1(100, s, beta)
+elif d == 2:
+    sc = scomplex.NGF_d2(100, s, beta)
+elif d == 3:
+    sc = scomplex.NGF_d3(100, s, beta)
+
+plotting.plot_complex(sc, ax1)
+ax1.set_title(r"\textbf{NGF d = " + str(d) + ", s = " + str(s) + "}", fontsize=14)
+for i in range(d):
+    ax = axv[i]
+    for j in range(d + 1):
+        lab = "L" + str(j)
+        #plot_mean(
+        #    1 - Ns[:, j, :] / N, deg_distance[:, j, i, :], palette[j, :], ls, ax, lab
+        #)
+        if ls:
+            for r in range(rep):
+                ax.semilogy(
+                    np.squeeze(1 - Ns[r, j, :]/N),
+                    np.squeeze(deg_distance[r, j, i, :]),
+                    linewidth=1,
+                    color=palette[j, :],
+                    alpha=0.3,
+                )
+        else:
+            for r in range(rep):
+                ax.plot(
+                    1 - Ns[r, j, :] / N,
+                    deg_distance[r, j, i, :],
+                    linewidth=1,
+                    color=palette[j, :],
+                    alpha=0.3,
+                )
+
+    ax.legend()
+    ax.set_title(r"\textbf{" + names[i] + "-" + names[d] + "}", fontsize=14)
+    #ax.set_xlim([0, 0.8])
+
 plt.show()
+plt.savefig(path + "/deg_errors.pdf", format="pdf", bbox_inches="tight")